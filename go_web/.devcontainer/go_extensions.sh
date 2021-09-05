5df37f0c6a10024921663af8a184e975cf403136
Gustavo Elizarraras <angelizarrarasg@gmail.com>
2021-09-05 16:22:14 +0000

    Development enviroment

---
+++ go_web/.devcontainer/go_extensions.sh
@@ -0,0 +1,10 @@
+#!bin/sh
+go get -v golang.org/x/tools/gopls
+go get -v github.com/ramya-rao-a/go-outline
+go get -v github.com/uudashr/gopkgs/v2/cmd/gopkgs
+go get -v golang.org/x/tools/cmd/goimports
+go get -v github.com/acroca/go-symbols
+go get -v golang.org/x/tools/cmd/guru
+go get -v golang.org/x/tools/cmd/gorename
+go get -v github.com/fatih/gomodifytags
+go get -v github.com/josharian/impl
