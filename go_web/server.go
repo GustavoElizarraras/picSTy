eade1cb19c4a4bd5ca284b4710a41bca3a048deb
Gustavo Elizarraras <angelizarrarasg@gmail.com>
2021-09-09 20:15:11 +0000

    send image from http

--- go_web/server.go
+++ go_web/server.go
@@ -1,11 +1,69 @@
-package main 
+package main
 
-import(
+import (
+	"fmt"
+	"io"
 	"net/http" // Client type por make requests and receive responses
-    "time"
+	"os"
+	"time"
 )
 
-client := &http.Client{
-	Timeout: 30 * time.Second,
+// type HelloHandler struct{}
+
+// func (hh HelloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+// 	w.Write([]byte("Hello!\n"))
+// }
+
+// type Page struct{
+// 	Title string
+// 	Body []byte
+// }
+
+// func loadPage(title string) {
+
+// }
+
+// func picstyHandler(w http.ResponseWriter, r *http.Request) {
+// 	// picsty, _:= ioutil.ReadFile("index.html")
+// 	t, _ := template.ParseFiles("index.html")
+// }
+
+// Uploading image
+func uploadImage(w http.ResponseWriter, r *http.Request) {
+	r.ParseMultipartForm(32 << 20)
+	file, handler, err := r.FormFile("imgFile")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+	fmt.Fprintf(w, "%v", handler.Header)
+	f, err := os.OpenFile("./test/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer f.Close()
+	io.Copy(f, file)
 }
 
+func main() {
+
+	s := http.Server{
+		Addr: ":8080",
+		// specifying timeouts, set these to handle malicious or broken HTTP clients
+		ReadTimeout:  30 * time.Second,
+		WriteTimeout: 90 * time.Second,
+		IdleTimeout:  120 * time.Second,
+		// Handler:      HelloHandler{},
+	}
+
+	http.Handle("/", http.FileServer(http.Dir(".")))
+	http.HandleFunc("/upload", uploadImage)
+	err := s.ListenAndServe()
+	if err != nil {
+		if err != http.ErrServerClosed {
+			panic(err)
+		}
+	}
+
+}
